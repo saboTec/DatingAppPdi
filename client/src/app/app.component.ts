7a47493fedbc9b140b5f605fd4cfd3d824c805e8
JoNoahBros <crnkic_sa20@hotmail.com>
2025-05-03 12:26:17 +0200

    update persistant login

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,21 +1,35 @@
 import { Component , inject, OnInit} from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { RouterOutlet } from '@angular/router';
-import { NgFor } from '@angular/common';
+import { NavComponent } from "./nav/nav.component";
+import { AccountService } from './_service/account.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [ NavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
   http = inject(HttpClient);
+  private accountService = inject(AccountService);
   title = 'DatingApp';
   users: any;
 
+
   ngOnInit(): void {
+    this.getUsers();
+    this.setCurrentUser();
+  }
+
+  setCurrentUser(){
+    const userString = localStorage.getItem('user');
+    if (!userString) return;
+    const user = JSON.parse(userString);
+    this.accountService.currentUser.set(user);
+  }
+  getUsers(){
     this.http.get('https://localhost:5001/api/users').subscribe({
       next: response => this.users = response,
       error: error =>console.log(error),
