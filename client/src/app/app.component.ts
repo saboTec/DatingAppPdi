e771f817647d2cb9dcee8cc49d69918ded3d316d
JoNoahBros <crnkic_sa20@hotmail.com>
2025-05-04 08:50:16 +0200

    update till cancelRegister

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -3,25 +3,28 @@ import { HttpClient } from '@angular/common/http';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from "./nav/nav.component";
 import { AccountService } from './_service/account.service';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from "./register/register.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ NavComponent],
+  imports: [HomeComponent, NavComponent, RouterOutlet, RegisterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
   http = inject(HttpClient);
   private accountService = inject(AccountService);
-  title = 'DatingApp';
+  // title = 'DatingApp';
   users: any;
 
 
   ngOnInit(): void {
-    this.getUsers();
+
     this.setCurrentUser();
   }
+
 
   setCurrentUser(){
     const userString = localStorage.getItem('user');
@@ -29,12 +32,6 @@ export class AppComponent implements OnInit {
     const user = JSON.parse(userString);
     this.accountService.currentUser.set(user);
   }
-  getUsers(){
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: response => this.users = response,
-      error: error =>console.log(error),
-      complete: () => console.log('Request has complited')
-    })
-  }
+
 }
  
