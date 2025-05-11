afcd0cb90dd5eb78ded591f22eee69baa988267b
Sabahudin Crnkic <crnkic_sa20@hotmail.com>
2025-05-11 11:06:40 +0200

    update member card

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { MembersService } from '../../_services/members.service';
+import { ActivatedRoute } from '@angular/router';
+import { Member } from '../../_models/member';
 
 @Component({
   selector: 'app-member-detail',
@@ -6,6 +9,22 @@ import { Component } from '@angular/core';
   templateUrl: './member-detail.component.html',
   styleUrl: './member-detail.component.css'
 })
-export class MemberDetailComponent {
+export class MemberDetailComponent implements OnInit{
+  
+  private memberService = inject(MembersService);
+  private route = inject(ActivatedRoute);
+  member? :Member;
+  
+  ngOnInit(): void {
+    this.loadMember();
+  }
+
+  loadMember(){
+    var username = this.route.snapshot.paramMap.get('username');
+    if(!username) return;
+    this.memberService.getMember(username).subscribe({
+      next : member => this.member = member
+    })
+  }
 
 }
