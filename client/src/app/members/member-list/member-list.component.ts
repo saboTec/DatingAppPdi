64148ab668d9b9881f6ff8ede8efa6354ea3a392
JoNoahBros <crnkic_sa20@hotmail.com>
2025-05-20 16:06:01 +0200

    end of section 13

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -16,25 +16,25 @@ import { ButtonsModule } from 'ngx-bootstrap/buttons';
   styleUrl: './member-list.component.css'
 })
 export class MemberListComponent implements OnInit {
+
   private accountService = inject(AccountService)
   memberService = inject(MembersService);
-  userParams = new UserParams(this.accountService.currentUser())
   genderList = [{value:'male',display:'Males'},{value:'female',display:'Females'}]
 
   ngOnInit(): void {
     if(!this.memberService.paginatedResult()) this.loadMembers();
   }
   loadMembers(){
-    this.memberService.getMembers(this.userParams)
+    this.memberService.getMembers()
   }
   resetFilters(){
-    this.userParams = new UserParams(this.accountService.currentUser());
+    this.memberService.resetUserParams();
     this.loadMembers();
   }
 
   pageChanged(event: any) {
-    if(this.userParams.pageNumber! = event.page){
-      this.userParams.pageNumber = event.page;
+    if(this.memberService.userParams().pageNumber! = event.page){
+      this.memberService.userParams().pageNumber = event.page;
       this.loadMembers();
     }
   }
